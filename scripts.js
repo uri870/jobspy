17a6f7b9f58531a75a08dd40caa2291bcf532d84
Uri W <uri870@gmail.com>
2024-11-23 18:01:13 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,7 +20,7 @@ async function handleAuth(event) {
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('errorMessage');
     const submitButton = document.getElementById('submitButton');
-    const endpoint = isLoginMode ? '/login.py' : '/register.py';
+    const endpoint = isLoginMode ? '/login' : '/register';
     
     errorMessage.textContent = '';
     submitButton.disabled = true;
